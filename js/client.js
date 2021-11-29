38dfc6ed7ed10f4e4599961dc9e5b9b03adf45e6
Tony Grimes <acidtone@tonygrimes.com>
2021-11-29 13:10:15 -0700

    added lunch functionality

--- js/client.js
+++ js/client.js
@@ -4,10 +4,12 @@ const init = async () => {
   const response = await fetch('https://graph.instagram.com/4445844762200108/media?fields=id,%20caption,link,media_url&access_token=IGQVJXb2dxeU9mRXNUT0NWM2xJZAnlFU3BoSkpfSDloV3dLa2M3VUE0TVlHUXJmVGVYM2VsaGxLczdRS0xGbUFaMjhqYW1fNGttdEdvRHpxa2F2Sk1seDYzUTRlcVhmd3ZALQTZAOdXlVUDl0ZA3ZACOFlTbgZDZD');
   const data = await response.json();
 
+  const lunchFigure = data.data.find((item) => item.caption.startsWith('#lunch'));
+
   document.querySelector('.container').innerHTML = `
     <figure>
-      <img src="${data[0].media_url}" alt="Tony's image">
-      <figcaption>${data.data[0].caption}</figcaption>
+      <img src="${lunchFigure.media_url}" alt="Tony's image">
+      <figcaption>${lunchFigure.caption}</figcaption>
     </figure>
   `
   console.log(data);
