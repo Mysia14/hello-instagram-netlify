96b4d3ede3b038e2f3e71e4404717d88e4ac127c
Tony Grimes <acidtone@tonygrimes.com>
2021-11-29 13:20:32 -0700

    added serverless function, hopefully

--- js/client.js
+++ js/client.js
@@ -1,7 +1,7 @@
 console.log('Hello World!');
 
 const init = async () => {
-  const response = await fetch('https://graph.instagram.com/4445844762200108/media?fields=id,%20caption,link,media_url&access_token=IGQVJXb2dxeU9mRXNUT0NWM2xJZAnlFU3BoSkpfSDloV3dLa2M3VUE0TVlHUXJmVGVYM2VsaGxLczdRS0xGbUFaMjhqYW1fNGttdEdvRHpxa2F2Sk1seDYzUTRlcVhmd3ZALQTZAOdXlVUDl0ZA3ZACOFlTbgZDZD');
+  const response = await fetch('/.netlify/functions/lunch');
   const data = await response.json();
 
   const lunchFigure = data.data.find((item) => item.caption.startsWith('#lunch'));
