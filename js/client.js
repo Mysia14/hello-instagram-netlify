40ec87c853f34ae045350f989078e56201ad51e1
Tony Grimes <acidtone@tonygrimes.com>
2021-11-29 13:00:55 -0700

    added output

--- js/client.js
+++ js/client.js
@@ -3,6 +3,13 @@ console.log('Hello World!');
 const init = async () => {
   const response = await fetch('https://graph.instagram.com/4445844762200108/media?fields=id,%20caption,link,media_url&access_token=IGQVJXb2dxeU9mRXNUT0NWM2xJZAnlFU3BoSkpfSDloV3dLa2M3VUE0TVlHUXJmVGVYM2VsaGxLczdRS0xGbUFaMjhqYW1fNGttdEdvRHpxa2F2Sk1seDYzUTRlcVhmd3ZALQTZAOdXlVUDl0ZA3ZACOFlTbgZDZD');
   const data = await response.json();
+
+  document.querySelector('.container').innerHTML = `
+    <figure>
+      <img src="${data[0].media_url}" alt="Tony's image">
+      <figcaption>${data.data[0].caption}</figcaption>
+    </figure>
+  `
   console.log(data);
 }
 
