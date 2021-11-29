691d3980d3046db4902655b0f1f6e9703fd875b9
Tony Grimes <acidtone@tonygrimes.com>
2021-11-29 13:23:48 -0700

    testing

--- js/client.js
+++ js/client.js
@@ -4,14 +4,14 @@ const init = async () => {
   const response = await fetch('/.netlify/functions/lunch');
   const data = await response.json();
 
-  const lunchFigure = data.data.find((item) => item.caption.startsWith('#lunch'));
+  // const lunchFigure = data.data.find((item) => item.caption.startsWith('#lunch'));
 
-  document.querySelector('.container').innerHTML = `
-    <figure>
-      <img src="${lunchFigure.media_url}" alt="Tony's image">
-      <figcaption>${lunchFigure.caption}</figcaption>
-    </figure>
-  `
+  // document.querySelector('.container').innerHTML = `
+  //   <figure>
+  //     <img src="${lunchFigure.media_url}" alt="Tony's image">
+  //     <figcaption>${lunchFigure.caption}</figcaption>
+  //   </figure>
+  // `
   console.log(data);
 }
 
